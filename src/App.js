d7a0df8ee779d2a8cd40e8789b42e1a463527044
Arnab Das <129678209+Arnabdas123456@users.noreply.github.com>
2024-01-18 09:43:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tictactoe from './Component/Tic-Tac-Toe/Tictactoe';
+
+function App() {
+  return (
+    <div>
+      <Tictactoe/>
+    </div>
+  );
+}
+
+export default App;
